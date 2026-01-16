0ccb5162bce9f385a2b180ab1e40e8d5055477af
Savannah <savannahvdkolk@outlook.com>
2026-01-16 10:38:35 +0100

    Set spell lang, clean comments

--- init.lua
+++ init.lua
@@ -6,19 +6,10 @@ require("config.lazy")
 -- require("nvim-treesitter.install").compilers = { "clang", "gcc" }
 
 -- register GLSL file types
--- vim.lsp.enable("glsl_analyzer")
--- vim.lsp.config("glsl_analyzer", {
---   filetypes = { "glsl", "vert", "tesc", "tese", "frag", "geom", "comp", "fsh", "vsh" },
--- })
--- require("lspconfig").glsl_analyzer.setup({})
 vim.treesitter.language.register("glsl", "vsh")
 vim.treesitter.language.register("glsl", "fsh")
 
 -- allow CTRL-backspace in Insert mode
 vim.api.nvim_set_keymap("i", "<C-H>", "<C-W>", { noremap = true })
 
--- set default working dir in /repos
---
---if vim.fn.expand('%:p') == ""
---vim.cmd("cd /users/ezrav/source/repos/")
--- print(vim.fn.expand("%:p"))
+vim.cmd("set spelllang=nl,en")
