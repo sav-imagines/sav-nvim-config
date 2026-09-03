7bfe4976a2e52924ca8e74ca0fb98e619b8c8817
Savannah <124691102+sav-imagines@users.noreply.github.com>
2026-09-03 18:38:37 +0200

    CsvView added nicely

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- autoload CsvView for [ct]sv files
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.csv", "*.tsv" },
+  callback = function()
+    require("csvview").enable()
+  end,
+  desc = "Enable CsvView on opening tsv files",
+})
