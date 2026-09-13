063542b43b669d002c05c558529cfe56bcbd2005
Savannah <savannahvdkolk@outlook.com>
2026-09-13 14:21:22 +0200

    Clean up old fixes

--- init.lua
+++ init.lua
@@ -1,14 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- set clang as default C compiler
--- this is no longer allowed?
--- require("nvim-treesitter.install").compilers = { "clang", "gcc" }
-
--- register GLSL file types
-vim.treesitter.language.register("glsl", "vsh")
-vim.treesitter.language.register("glsl", "fsh")
-
 -- allow CTRL-backspace in Insert mode
 vim.api.nvim_set_keymap("i", "<C-H>", "<C-W>", { noremap = true })
 
