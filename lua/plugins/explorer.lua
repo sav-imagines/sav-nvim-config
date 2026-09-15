587f7b08e41e195a98d1dd66d6960daece2643e7
Savannah <124691102+sav-imagines@users.noreply.github.com>
2026-09-15 11:07:04 +0200

    Projects window

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -16,6 +16,7 @@ return {
           { section = "keys", gap = 1, padding = 1 },
           { section = "startup" },
         },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
       },
     },
     explorer = {
